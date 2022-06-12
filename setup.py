d2f39a155a2231dff89b6d2bdf4c7ab0fa947ee0
Laligonda <laligonda@Laligondas-MacBook-Pro.local>
2022-06-12 13:39:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku123",
+    version="0.0.3",
+    description="ML project",
+    author="BHK",
+    packages=find_packages(),
+    license="MIT"
+)
